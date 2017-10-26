84a6ab94324600a79b5020925cb2ff1df154788e
Khaled Bouguettoucha <khaled.bouguettoucha@gmail.com>
2017-10-26 16:27:32 +0200

    Stil updating type to match with what we need

--- test/test/Test.java
+++ test/test/Test.java
@@ -2,6 +2,9 @@ package test;
 
 import metier.*;
 import util.ECharpente;
+import util.ECorpsMetier;
+import util.EStatut;
+import util.ETitre;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -16,13 +19,7 @@ public class Test {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
 
-        /* 4 projets terminés :
-                - 1 lotissement : 10 lots, 8 entreprises, 10 acteurs
-                refProjet, nom, surfaceTotale, avancement, dateFinEstimee, coutTotalEstime, termine, dateFinReelle, nbMaisons
-                - 1 immeuble : 10 appartements, 8 lots, 7 entreprises, 8 acteurs,
-                - 1 Hôpital : 10 lots, 9 entreprises, 11 acteurs
-                - 1 Maison : 5 lots, 4 entreprises, 4 acteurs
-        */
+        // Un lotissement : 10 lots, 8 entreprises, 10 acteurs
 
         Lotissement lotissement = new Lotissement("Lotissement10Lots8Entreprises10Acteurs",
                 "Lotissement les Fleurs Bleues", 2000, "Terminé", new Date(2017-12-12),
@@ -36,12 +33,52 @@ public class Test {
         Menuiseries me = new Menuiseries(8, 15, 2);
         Peinture p = new Peinture(20);
         Platerie pl = new Platerie(200, 145);
-        Reseaux r = new Reseaux(150.57f);
+        Reseaux r = new Reseaux(150.67f);
         Terrassement t = new Terrassement(1600);
 
-        Adresse adresse = new Adresse(9, "rue de l'Europe", 70000, "Noidans-lès-Vesoul");
+        // Un immeuble : 10 appartements, 8 lots, 7 entreprises, 8 acteurs
 
-        em.persist(adresse);
+        // Un hôpital : 10 lots, 9 entreprises, 11 acteurs
+
+
+        // Une maison : 5 lots, 4 entreprises, 4 acteurs
+
+        Maison maison = new Maison("Maison5Lots",
+                "Pavillon 5 personnes", 150, "Terminé", new Date(2017-12-01),
+                400000, true, new Date(2017-10-11), 5, 2);
+
+        Charpente c2 = new Charpente(ECharpente.traditionnelle);
+        Dallage d2 = new Dallage(150);
+        Electricite e2 = new Electricite(2, 88.57f);
+        Fondation f2 = new Fondation(5);
+        Maconnerie m2 = new Maconnerie(500, 4);
+        Reseaux r2 = new Reseaux(73.67f);
+
+        // Entreprises et acteurs communs à tous les projets
+
+        Entreprise e1 = new Entreprise("Charpentiers passionnés", ECorpsMetier.Charpentier, "0694758455");
+        Entreprise e2 = new Entreprise("Carreleurs au grand coeur", ECorpsMetier.Carreleur, "0694758454");
+        Entreprise e3 = new Entreprise("Electriciens de demain", ECorpsMetier.Electricien, "0694758453");
+        Entreprise e4 = new Entreprise("Maçonnerie de famille", ECorpsMetier.Macon, "0694758452");
+        Entreprise e5 = new Entreprise("Menuisiers experts", ECorpsMetier.Menuisier, "0694758451");
+        Entreprise e6 = new Entreprise("Van Gogh peinture", ECorpsMetier.Peintre, "0694758450");
+        Entreprise e7 = new Entreprise("Le Plâtre depuis 1955", ECorpsMetier.Platrier, "0694758492");
+        Entreprise e8 = new Entreprise("Canalisations attention", ECorpsMetier.Plombier, "0694758405");
+        Entreprise e9 = new Entreprise("Gruss architecture", ECorpsMetier.Macon, "0694658405");
+
+        Acteur a1 = new Acteur("Jean François", ETitre.Technicien, EStatut.ChefEquipe);
+        Acteur a2 = new Acteur("Martin Daniel", ETitre.Technicien, EStatut.Employe);
+        Acteur a3 = new Acteur("Tarek Atri", ETitre.Technicien, EStatut.Employe);
+        Acteur a4 = new Acteur("Feng Chao", ETitre.Technicien, EStatut.Ouvrier);
+        Acteur a5 = new Acteur("Gilles Dupont", ETitre.Technicien, EStatut.Ouvrier);
+        Acteur a6 = new Acteur("Geoffrey Almeida", ETitre.Technicien, EStatut.Employe);
+        Acteur a7 = new Acteur("Mamadou Cissokho", ETitre.Technicien, EStatut.Employe);
+        Acteur a8 = new Acteur("Yeray Lopez", ETitre.Technicien, EStatut.Employe);
+        Acteur a9 = new Acteur("Camille Martin", ETitre.Technicien, EStatut.ChefEquipe);
+        Acteur a10 = new Acteur("Vanessa Durand", ETitre.Ingenieur, EStatut.Responsable);
+        Acteur a11 = new Acteur("Arlette Gruss", ETitre.Architecte, EStatut.Directeur);
+
+        //em.persist();
         em.getTransaction().commit();
         em.close();
         emf.close();
