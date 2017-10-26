0033e53199e58fd6f80b30a04563004722841b9e
Khaled Bouguettoucha <khaled.bouguettoucha@gmail.com>
2017-10-26 17:55:46 +0200

    coding 4 finished projects

--- test/test/Test.java
+++ test/test/Test.java
@@ -14,7 +14,6 @@ import java.sql.Date;
 public class Test {
     public static void main(String [] args){
 
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("tpersistancePU");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
@@ -32,51 +31,74 @@ public class Test {
         Maconnerie m = new Maconnerie(150, 6);
         Menuiseries me = new Menuiseries(8, 15, 2);
         Peinture p = new Peinture(20);
-        Platerie pl = new Platerie(200, 145);
+        Platerie pl = new Platerie(200, 1450);
         Reseaux r = new Reseaux(150.67f);
         Terrassement t = new Terrassement(1600);
 
         // Un immeuble : 10 appartements, 8 lots, 7 entreprises, 8 acteurs
 
+        Immeuble immeuble = new Immeuble(12);
+
+        Charpente c2 = new Charpente(ECharpente.fermette);
+        Dallage d2 = new Dallage(2000);
+        Electricite e2 = new Electricite(9, 91.57f);
+        Fondation f2 = new Fondation(13);
+        Maconnerie m2 = new Maconnerie(1500, 8);
+        Menuiseries me2 = new Menuiseries(53, 25, 8);
+        Platerie pl2 = new Platerie(1500, 6000);
+        Reseaux r2 = new Reseaux(99.97f);
+
         // Un hôpital : 10 lots, 9 entreprises, 11 acteurs
 
+        Hopital hopital = new Hopital(357, 24);
+
+        Charpente c3 = new Charpente(ECharpente.toitPlat);
+        Dallage d3 = new Dallage(20000);
+        Electricite e3 = new Electricite(12, 101.57f);
+        Fondation f3 = new Fondation(25);
+        Maconnerie m3 = new Maconnerie(1500, 22);
+        Menuiseries me3 = new Menuiseries(113, 55, 0);
+        Peinture p3 = new Peinture(25000);
+        Platerie pl3 = new Platerie(2000, 8500);
+        Reseaux r3 = new Reseaux(1510.97f);
+        Terrassement t3 = new Terrassement(65000);
 
         // Une maison : 5 lots, 4 entreprises, 4 acteurs
 
         Maison maison = new Maison("Maison5Lots",
-                "Pavillon 5 personnes", 150, "Terminé", new Date(2017-12-01),
+                "Pavillon 5 personnes", 150, "Terminé", new Date(2017-12-1),
                 400000, true, new Date(2017-10-11), 5, 2);
 
-        Charpente c2 = new Charpente(ECharpente.traditionnelle);
-        Dallage d2 = new Dallage(150);
-        Electricite e2 = new Electricite(2, 88.57f);
-        Fondation f2 = new Fondation(5);
-        Maconnerie m2 = new Maconnerie(500, 4);
-        Reseaux r2 = new Reseaux(73.67f);
+        Charpente c4 = new Charpente(ECharpente.traditionnelle);
+        Dallage d4 = new Dallage(150);
+        Electricite e4 = new Electricite(2, 88.57f);
+        Fondation f4 = new Fondation(5);
+        Maconnerie m4 = new Maconnerie(500, 4);
+        Reseaux r4 = new Reseaux(73.67f);
 
         // Entreprises et acteurs communs à tous les projets
 
-        Entreprise e1 = new Entreprise("Charpentiers passionnés", ECorpsMetier.Charpentier, "0694758455");
-        Entreprise e2 = new Entreprise("Carreleurs au grand coeur", ECorpsMetier.Carreleur, "0694758454");
-        Entreprise e3 = new Entreprise("Electriciens de demain", ECorpsMetier.Electricien, "0694758453");
-        Entreprise e4 = new Entreprise("Maçonnerie de famille", ECorpsMetier.Macon, "0694758452");
-        Entreprise e5 = new Entreprise("Menuisiers experts", ECorpsMetier.Menuisier, "0694758451");
-        Entreprise e6 = new Entreprise("Van Gogh peinture", ECorpsMetier.Peintre, "0694758450");
-        Entreprise e7 = new Entreprise("Le Plâtre depuis 1955", ECorpsMetier.Platrier, "0694758492");
-        Entreprise e8 = new Entreprise("Canalisations attention", ECorpsMetier.Plombier, "0694758405");
-        Entreprise e9 = new Entreprise("Gruss architecture", ECorpsMetier.Macon, "0694658405");
+        Entreprise ent1 = new Entreprise("Charpentiers passionnés", ECorpsMetier.Charpentier, "0694758455");
+        Entreprise ent2 = new Entreprise("Carreleurs au grand coeur", ECorpsMetier.Carreleur, "0694758454");
+        Entreprise ent3 = new Entreprise("Electriciens de demain", ECorpsMetier.Electricien, "0694758453");
+        Entreprise ent4 = new Entreprise("Maçonnerie de famille", ECorpsMetier.Macon, "0694758452");
+        Entreprise ent5 = new Entreprise("Menuisiers experts", ECorpsMetier.Menuisier, "0694758451");
+        Entreprise ent6 = new Entreprise("Van Gogh peinture", ECorpsMetier.Peintre, "0694758450");
+        Entreprise ent7 = new Entreprise("Le Plâtre depuis 1955", ECorpsMetier.Platrier, "0694758492");
+        Entreprise ent8 = new Entreprise("Canalisations attention", ECorpsMetier.Plombier, "0694758405");
+        Entreprise ent9 = new Entreprise("Gruss architecture", ECorpsMetier.Macon, "0694658405");
 
-        Acteur a1 = new Acteur("Jean François", ETitre.Technicien, EStatut.ChefEquipe);
-        Acteur a2 = new Acteur("Martin Daniel", ETitre.Technicien, EStatut.Employe);
-        Acteur a3 = new Acteur("Tarek Atri", ETitre.Technicien, EStatut.Employe);
-        Acteur a4 = new Acteur("Feng Chao", ETitre.Technicien, EStatut.Ouvrier);
-        Acteur a5 = new Acteur("Gilles Dupont", ETitre.Technicien, EStatut.Ouvrier);
-        Acteur a6 = new Acteur("Geoffrey Almeida", ETitre.Technicien, EStatut.Employe);
-        Acteur a7 = new Acteur("Mamadou Cissokho", ETitre.Technicien, EStatut.Employe);
-        Acteur a8 = new Acteur("Yeray Lopez", ETitre.Technicien, EStatut.Employe);
-        Acteur a9 = new Acteur("Camille Martin", ETitre.Technicien, EStatut.ChefEquipe);
-        Acteur a10 = new Acteur("Vanessa Durand", ETitre.Ingenieur, EStatut.Responsable);
-        Acteur a11 = new Acteur("Arlette Gruss", ETitre.Architecte, EStatut.Directeur);
+        Acteur act1 = new Acteur("Jean François", ETitre.Technicien, EStatut.ChefEquipe);
+        Acteur act2 = new Acteur("Martin Daniel", ETitre.Technicien, EStatut.Employe);
+        Acteur act3 = new Acteur("Tarek Atri", ETitre.Technicien, EStatut.Employe);
+        Acteur act4 = new Acteur("Feng Chao", ETitre.Technicien, EStatut.Ouvrier);
+        Acteur act5 = new Acteur("Gilles Dupont", ETitre.Technicien, EStatut.Ouvrier);
+        Acteur act6 = new Acteur("Geoffrey Almeida", ETitre.Technicien, EStatut.Employe);
+        Acteur act7 = new Acteur("Mamadou Cissokho", ETitre.Technicien, EStatut.Employe);
+        Acteur act8 = new Acteur("Yeray Lopez", ETitre.Technicien, EStatut.Employe);
+        Acteur act9 = new Acteur("Camille Martin", ETitre.Technicien, EStatut.ChefEquipe);
+        Acteur act10 = new Acteur("Vanessa Durand", ETitre.Ingenieur, EStatut.Responsable);
+        Acteur act11 = new Acteur("Arlette Gruss", ETitre.Architecte, EStatut.Directeur);
 
         //em.persist();
         em.getTransaction().commit();
