747ca2839f5ac59d6e1cdad39447c6e966f8c4c8
BERTRAND Guillaume <guillaume5524@hotmail.fr>
2017-10-29 09:00:07 +0100

    Add requests expectations

--- test/test/Test.java
+++ test/test/Test.java
@@ -16,6 +16,21 @@ public class Test {
 
         Seeder.initialize(em);
 
+        // Requetes
+        // 1 - Quelles sont les entreprises avec lesquelles le cabinet travaille ?
+        // 2 - Quels sont les projets en cours ?
+        // 3 - Quel est lʼavancement du projet de reference « PLot12 » ?
+        // 4 - Combien de projets portant sur un etablissement scolaire ont ete realise ?
+        // 5 - Quelles sont les entreprises de plomberie ?
+        // 6 - Quels sont les noms des contacts de lʼentreprise « General Batiment» ?
+        // 7 - A quels projets termines lʼentreprise « General Batiment» a participe ?
+        // 8 - Quels sont les lots des projets en cours auxquels participe lʼentreprise « General Batiment» ?
+        // 9 - Quels sont les acteurs (et leur entreprise) participant au projet de reference « PLot12 » ?
+        // 10 - Combien de lots a le projet de reference " PLot12 " ?
+        // 11 - Quel est le cout total estime du projet de reference « PLot12 »?
+        // 12 - Quelles sont les entreprises (et leur adresse) qui ont realisees les menuiseries dans les projets de Musee ?
+        // 13 - Quelles sont les durees estimees des differents projets en cours ?
+        // 14 - Quels sont les avancements des lots (et leur type) du projet de reference « PLot12 » ?
 
         em.getTransaction().commit();
         em.close();
